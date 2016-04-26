246c5f064e4c49e37ae432cec4650bf9faec8151
AaronTao1990 <taojinqiu1990@gmail.com>
2016-04-26 17:49:57 +0800

    add qunar bbs

--- lvyou/lvyou/spiders/qunar.py
+++ lvyou/lvyou/spiders/qunar.py
@@ -3,6 +3,7 @@ import scrapy
 from scrapy.http import Request
 from scrapy.selector import Selector
 import json
+import re
 
 class QunarSpider(scrapy.Spider):
     name = "qunar_gonglue"
@@ -50,5 +51,81 @@ class QunarSpider(scrapy.Spider):
                 'days' : days,
                 'content' : content
             }
-            #self.logger.info('攻略下载 : %s\t%s\t%s' % (name, download_count, meta['mark']))
             self.logger.info('qunar gonglue : %s' % json.dumps(result, ensure_ascii=False).encode('utf-8'))
+
+
+class QunarBBSSpider(scrapy.Spider):
+    name = 'qunar_bbs'
+
+    bbs_api = (
+        (u'聪明旅行家专区', u'分享', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=56&filter=typeid&typeid=28&orderby=views&page=%d', 13),
+        (u'聪明旅行家专区', u'组团', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=56&filter=typeid&typeid=29&orderby=views&page=%d', 17),
+        (u'旅游问答', u'国内', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=54&filter=typeid&typeid=17&orderby=views&page=%d', 577),
+        (u'旅游问答', u'东南亚', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=54&filter=typeid&typeid=18&orderby=views&page=%d', 135),
+        (u'旅游问答', u'欧洲', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=54&filter=typeid&typeid=19&orderby=views&page=%d', 112),
+        (u'旅游问答', u'美洲', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=54&filter=typeid&typeid=20&orderby=views&page=%d', 29),
+        (u'旅游问答', u'大洋洲', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=54&filter=typeid&typeid=21&orderby=views&page=%d', 24),
+        (u'精彩游记', u'国内游', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=42&typeid=3&orderby=views&typeid=3&orderby=views&filter=typeid&page=%d', 168),
+        (u'精彩游记', u'港澳台', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=42&filter=typeid&typeid=4&orderby=views&page=%d', 12),
+        (u'精彩游记', u'出境游', 'http://travel.qunar.com/bbs/forum.php?mod=forumdisplay&fid=42&filter=typeid&typeid=5&orderby=views&page=%d', 52),
+    )
+
+    HEADERS = {
+        'Host' : 'travel.qunar.com',
+        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding' : 'gzip, deflate, sdch',
+        'Accept-Language' : 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2,ja;q=0.2'
+    }
+
+    def start_requests(self):
+        for gonglue_item in self.bbs_api:
+            meta = {}
+            meta['main_class'] = gonglue_item[0]
+            meta['second_class'] = gonglue_item[1]
+            for page in range(gonglue_item[3]):
+                yield Request(gonglue_item[2] % (page + 1), meta=meta, headers=self.HEADERS, dont_filter=True, callback=self.parse_list)
+                return
+
+    def parse_list(self, response):
+        meta = response.meta
+        body = re.sub(r'(<tbody id="normalthread_\d+")', r'\g<1>>', response.body_as_unicode())
+        selector = Selector(text=body)
+        for tiezi in selector.xpath('//table[@id="threadlisttableid"]/tbody[position()>1]/tr/th'):
+            title = tiezi.xpath('./span[@class="xst"]/a/text()').extract_first()
+            author = tiezi.xpath('./p[@class="mtn xg1"]/a[1]/text()').extract_first()
+            date = tiezi.xpath('./p[@class="mtn xg1"]/span/text()').extract_first()
+            view_count_text = ''.join(tiezi.xpath('./p[@class="mtn xg1"]/node()').extract())
+            view_count = re.search(u'(?<=查看: )\d+', view_count_text).group(0)
+            reply_count = tiezi.xpath('./p[@class="mtn xg1"]/a[2]/text()').extract_first()
+            url = 'http://travel.qunar.com/bbs/' + tiezi.xpath('./span[@class="xst"]/a/@href').extract_first()
+            data = {
+                'main_class' : meta['main_class'],
+                'second_class' : meta['second_class'],
+                'title' : title,
+                'author' : author,
+                'date' : date,
+                'view_count' : view_count,
+                'reply_count' : reply_count,
+                'url' : url
+            }
+            yield Request(url, meta={'data':data}, headers=self.HEADERS, dont_filter=True, callback=self.parse_content)
+
+    def parse_content(self, response):
+        data = response.meta['data']
+        selector = Selector(response)
+        replies = []
+        for post in selector.xpath('//div[@id="postlist"]//td[@class="plc"]'):
+            time = post.xpath('.//div[@class="authi"]/em/text()').extract_first()
+            content = ''.join(post.xpath('.//div[@class="t_fsz"]/node()').extract())
+            if not time or not content:
+                continue
+            replies.append({
+                'time' : time,
+                'content' : content
+            })
+        if replies:
+            data['content'] = replies[0]['content']
+            data['replies'] = replies[1:]
+        self.logger.info('qunar bbs: %s' % json.dumps(data, ensure_ascii=False).encode('utf-8'))
+
+
