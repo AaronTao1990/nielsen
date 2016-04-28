1c0bd3faac29336d1f59bdb5834c63b1157e0411
Aaron <aarontao1990@gmail.com>
2016-04-28 22:59:18 +0800

    add classes for baidu spider

--- lvyou/lvyou/spiders/baidu.py
+++ lvyou/lvyou/spiders/baidu.py
@@ -277,6 +277,9 @@ class BaiduMudidiGonglueSpider(scrapy.Spider):
                 favorite_count = item.get('favorite_count')
                 days = item.get('days')
                 result = {
+                    'first_class' : meta['first_class'],
+                    'second_class' : meta['second_class'],
+                    'third_class' : meta['third_class'],
                     'title' : title,
                     'author' : author,
                     'destinations' : destinations,
