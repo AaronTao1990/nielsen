6b3a7bc53f026dd1fc5bdea5f302b84bf244fdb5
AaronTao1990 <taojinqiu1990@gmail.com>
2016-04-28 18:20:52 +0800

    comment out ctrip youji spider

--- lvyou/lvyou/spiders/ctrip.py
+++ lvyou/lvyou/spiders/ctrip.py
@@ -81,37 +81,31 @@ class CtripSpider(scrapy.Spider):
             #self.logger.info('攻略下载 : %s\t%s\t%s' % (name, download_count, meta['mark']))
             self.logger.info('ctrip gonglue : %s' % json.dumps(result, ensure_ascii=False).encode('utf-8'))
 
-class CtripYoujiSpider(scrapy.Spider):
-    name = "ctrip_youji"
-
-    youji_api = ('http://you.ctrip.com/TravelSite/Home/IndexTravelListHtml?p=%d&Idea=0&Type=1&Plate=0', 500)
-    comments_api = ('http://you.ctrip.com/TravelSite/Home/TravelReplyListHtml?TravelId=2903586&IsReplyRefresh=0&ReplyPageNo=5&ReplyPageSize=10&_=1460903593336')
-
-    def start_requests(self):
-        url, total_pages = self.youji_api
-        for page in range(total_pages):
-            yield Request(url % (page+1), callback=self.parse_youji)
-
-    def parse_youji(self, response):
-        selector = Selector(response)
-        for youji in selector.xpath('//div[@class="city"]'):
-            city_name = youji.xpath('./div[@class="city-sub"]/a[@class="city-name"]/text()').extract_first()
-            title = youji.xpath('./div[@class="city-sub"]/a[@class="cpt"]/text()').extract_first()
-            view_count = youji.xpath('./div[@class="city-sub"]/p[@class="opts"]/i[@class="numview"]/text()').extract_first()
-            love_count = youji.xpath('./div[@class="city-sub"]/p[@class="opts"]/i[@class="want"]/text()').extract_first()
-            comment_count = youji.xpath('./div[@class="city-sub"]/p[@class="opts"]/i[@class="numreply"]/text()').extract_first()
-            meta = {
-                'city_name' : city_name,
-                'title' : title,
-                'view_count' : view_count,
-                'love_count' : love_count,
-                'comment_count' : comment_count
-            }
-            print json.dumps(meta, ensure_ascii=False).encode('utf-8')
-
-#class CtripGentuanSpider(scrapy.Spider):
-#    name = 'ctrip_gentuan'
+#class CtripYoujiSpider(scrapy.Spider):
+#    name = "ctrip_youji"
 #
-#    get_dest_key_list_api = 'http://vacations.ctrip.com/grouptravel/'
-#    get_dest_list_api = 'http://vacations.ctrip.com/Package-Booking-VacationsOnlineSiteUI/Handler/HotDestKeysNew.ashx?requestType=0&startCity=1&module=indexV2'
-
+#    youji_api = ('http://you.ctrip.com/TravelSite/Home/IndexTravelListHtml?p=%d&Idea=0&Type=1&Plate=0', 500)
+#    comments_api = ('http://you.ctrip.com/TravelSite/Home/TravelReplyListHtml?TravelId=2903586&IsReplyRefresh=0&ReplyPageNo=5&ReplyPageSize=10&_=1460903593336')
+#
+#    def start_requests(self):
+#        url, total_pages = self.youji_api
+#        for page in range(total_pages):
+#            yield Request(url % (page+1), callback=self.parse_youji)
+#
+#    def parse_youji(self, response):
+#        selector = Selector(response)
+#        for youji in selector.xpath('//div[@class="city"]'):
+#            city_name = youji.xpath('./div[@class="city-sub"]/a[@class="city-name"]/text()').extract_first()
+#            title = youji.xpath('./div[@class="city-sub"]/a[@class="cpt"]/text()').extract_first()
+#            view_count = youji.xpath('./div[@class="city-sub"]/p[@class="opts"]/i[@class="numview"]/text()').extract_first()
+#            love_count = youji.xpath('./div[@class="city-sub"]/p[@class="opts"]/i[@class="want"]/text()').extract_first()
+#            comment_count = youji.xpath('./div[@class="city-sub"]/p[@class="opts"]/i[@class="numreply"]/text()').extract_first()
+#            meta = {
+#                'city_name' : city_name,
+#                'title' : title,
+#                'view_count' : view_count,
+#                'love_count' : love_count,
+#                'comment_count' : comment_count
+#            }
+#            print json.dumps(meta, ensure_ascii=False).encode('utf-8')
+#
