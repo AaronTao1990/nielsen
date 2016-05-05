e0f8328156a7e66d5fc31748819e1634105268f5
AaronTao1990 <taojinqiu1990@gmail.com>
2016-05-05 16:29:00 +0800

    update

--- sogou_crawler/task_publisher/keywords_generator.py
+++ sogou_crawler/task_publisher/keywords_generator.py
@@ -1,4 +1,5 @@
 from optparse import OptionParser
+from netcall import fetch_html
 import redis
 import logging
 
@@ -7,7 +8,8 @@ logger = logging.getLogger(__name__)
 redis_config = {
     'host' : '10.111.0.12',
     'port' : 6380,
-    'ntasks' : 'ntasks'
+    'ntasks' : 'ntasks',
+    'proxy' : 'proxy'
 }
 
 
@@ -31,12 +33,28 @@ class KeywordsGenerator(object):
         except Exception, e:
             logger.error('exception : %s' % e)
 
-def main(filename, target, args):
+    def publish_proxies(self):
+        api = 'http://api.zdaye.com/?api=201605051157596943&pw=123&checktime=1%D0%A1%CA%B1%C4%DA&gb=2'
+        resp = fetch_html(api)
+        try:
+            for line in resp.split('\n'):
+                proxy = 'http://' + line
+                self.redis_cli.sadd(redis_config['proxy'], proxy)
+        except Exception:
+            pass
+
+def main(cmd, filename, target, args):
     generator = KeywordsGenerator(filename, target)
-    generator.publish_tasks()
+    if cmd == 'proxy':
+        generator.publish_proxies()
+    else:
+        generator.publish_tasks()
 
 if __name__ == '__main__':
     parser = OptionParser()
+    parser.add_option('-c', '--cmd', dest='cmd',
+                      default='proxy',
+                      help='command')
     parser.add_option('-f', '--filename', dest='filename',
                       default='source.txt',
                       help='filename')
@@ -44,5 +62,5 @@ if __name__ == '__main__':
                       default='ntasks',
                       help='target')
     (options, args) = parser.parse_args()
-    main(options.filename, options.target, args)
+    main(options.cmd, options.filename, options.target, args)
 
